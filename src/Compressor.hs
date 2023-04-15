27cf3c38e2c5a73977832b96b1e0c054fdcb9636
malcom <malcom@senoee.com>
2023-04-15 11:42:22 +0200

    [ADD] - parsing for cluster handling

--- src/Compressor.hs
+++ src/Compressor.hs
@@ -17,26 +17,40 @@ import System.Random
 import Data.List
 import Data.Char
 
--- parseString :: String -> [Int]
--- parseString xs = case dropWhile (not . isDigit) xs of
---                      "" -> []
---                      ys -> let (n, rest) = span isDigit ys
---                            in read n : parseString rest
+parseString :: String -> [Int]
+parseString xs = case dropWhile (not . isDigit) xs of
+                     "" -> []
+                     ys -> let (n, rest) = span isDigit ys
+                           in read n : parseString rest
 
--- readPixels :: [String] -> [[Int]]
--- readPixels [] = []
--- readPixels (x:xs) = parseString x : readPixels xs
+readPixels :: [String] -> [[Int]]
+readPixels [] = []
+readPixels (x:xs) = parseString x : readPixels xs
+
+fillPoint ::  [Int] -> Point
+fillPoint (r:g:b) = (r,g,last b)
+
+fillPixel :: [Int] -> Pixel
+fillPixel (x:xs:xxs) = ((x, xs), fillPoint xxs)
+
+fillCluster :: [[Int]] -> Cluster
+fillCluster [] = []
+fillCluster (x:xs) = fillPixel x : fillCluster xs
 
 -- addEmptyList :: [[Int]] -> Int -> [[[Int]]]
 -- addEmptyList list 0 = [list]
 -- addEmptyList list i = [] : addEmptyList list (i - 1)
 
+
 getPixels :: [String] -> Cluster
-getPixels content = [] -- TODO
+getPixels [] = []
+getPixels line = do
+    let list = readPixels line
+    getPixels line
 
 startCompressor :: Int -> Double -> String -> StdGen -> IO ()
 startCompressor a b c gen = if (errorHandlingValues a b c) == False then exitWith(ExitFailure 84) else do
-    
+
     content <- readFile c
     let myLines = lines content
     let pixels = getPixels myLines
